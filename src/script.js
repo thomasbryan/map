14ac7733382b158b5cae36336b4e7ae15c0df800
Thomas Bryan <tbryan@axxess.com>
2016-08-10 05:01:08 -0500

    add styles and bad logic

--- src/script.js
+++ src/script.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
   $.post("/api/users",auth)
   .done(function(res) {
     app = res;
+    $('#name').html(app.name);
+    var pad = "";
+    if(app.lvl + 1 < 10 ) pad = "0";
+    $('#lvl').html(pad+(app.lvl + 1));
     app.id = auth.user;
     app.map = []
     map();
@@ -29,6 +33,7 @@ function map() {
     , c = p(app.pos.c)
     , d = p(app.pos.d)
     ;
+// preload next map grid
   if(app.map[a+b+c+d] === undefined){
     $.get('/map/'+a+b+'/'+c+d,function(res) {
       app.map[a+b+c+d] = res;
@@ -39,16 +44,95 @@ function map() {
     var rows = app.map[a+b+c+d].toString().split("\n")
       , html = ""
       ;
+    rows.pop();//remove extra line
 //TODO handle edge cases
+//TODO DRY!!! fix logic!
     for(var i = app.pos.f - 2;i <= app.pos.f + 2;i++) {
-      var cols = rows[i].toString().split(',');
-      html+="<div class='row'>";
-      for(var j = app.pos.e - 2;j <= app.pos.e + 2;j++) {
-        html+="<div class='col tile-"+cols[j]+"'></div>";
+      if(i < 0 || i > 24) {
+        html+="<div class='row'>";
+        if(i < 0) {
+          c = p(app.pos.c-1);
+          if( app.map[a+b+c+d] === undefined) {
+            $.get('/map/'+a+b+'/'+c+d,function(res) {
+              app.map[a+b+c+d] = res;
+              map();
+            });
+          }else{
+            var up = app.map[a+b+c+d].toString().split("\n");
+            up.pop();
+            var cols = rows[i+24].toString().split(',');
+            for(var j = app.pos.e - 2;j <= app.pos.e + 2;j++) {
+              if(j < 0 || j > 24) {
+                html+="<div class='col tile-x'>edge</div>";
+              }else{
+                html+="<div class='col tile-"+cols[j]+"'></div>";
+              }
+            }
+          }
+        }else{
+          c = p(app.pos.c+1);
+          if( app.map[a+b+c+d] === undefined) {
+            $.get('/map/'+a+b+'/'+c+d,function(res) {
+              app.map[a+b+c+d] = res;
+              map();
+            });
+          }else{
+            var down = app.map[a+b+c+d].toString().split("\n");
+            down.pop();
+            var cols = rows[i-24].toString().split(',');
+            for(var j = app.pos.e - 2;j <= app.pos.e + 2;j++) {
+              if(j < 0 || j > 24) {
+                html+="<div class='col tile-x'>edge</div>";
+              }else{
+                html+="<div class='col tile-"+cols[j]+"'></div>";
+              }
+            }
+          }
+        }
+        html+="</div>";
+      }else{
+        var cols = rows[i].toString().split(',');
+        html+="<div class='row'>";
+        for(var j = app.pos.e - 2;j <= app.pos.e + 2;j++) {
+          if(j < 0 || j > 24) {
+            if(j<0) {
+
+          d = p(app.pos.d-1);
+          if( app.map[a+b+c+d] === undefined) {
+            $.get('/map/'+a+b+'/'+c+d,function(res) {
+              app.map[a+b+c+d] = res;
+              map();
+            });
+          }else{
+            var left = app.map[a+b+c+d].toString().split("\n");
+            left.pop();
+            var leftcols = rows[i].toString().split(',');
+            html+="<div class='col tile-"+leftcols[j+24]+"'></div>";
+          }
+            }else{
+          d = p(app.pos.d+1);
+          if( app.map[a+b+c+d] === undefined) {
+            $.get('/map/'+a+b+'/'+c+d,function(res) {
+              app.map[a+b+c+d] = res;
+              map();
+            });
+          }else{
+            var right = app.map[a+b+c+d].toString().split("\n");
+            right.pop();
+            var rightcols = rows[i].toString().split(',');
+            html+="<div class='col tile-"+rightcols[j-24]+"'></div>";
+          }
+
+            }
+          }else{
+            html+="<div class='col tile-"+cols[j]+"'></div>";
+          }
+        }
+        html+="</div>";
       }
-      html+="</div>";
     }
     $('#map').html(html);
+    $('#name').html(app.name+'<br />'+app.pos.e+'/'+app.pos.f);
   }
 }
 function move(req) {
